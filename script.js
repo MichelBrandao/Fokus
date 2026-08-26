355ed38077deec7690c0445fc96a83323146984f
michel brandao <michelbrandaog@gmail.com>
2026-08-25 22:34:21 -0300

    config. audios ao iniciar, pausar e quando finalzair o timer no script.js

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
 const html = document.querySelector('html')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
-
 const botoes = document.querySelectorAll('.app__card-button')
-const btn_iniciar = document.querySelector('.app__card-primary-button');
 const btn_foco = document.querySelector('.app__card-button--foco')
 const btn_curto = document.querySelector('.app__card-button--curto')
 const btn_longo = document.querySelector('.app__card-button--longo')
-
-const musica_toggle = document.querySelector('#alternar-musica')
-const musica = new Audio('./sons/luna-rise-part-one.mp3')
-
-const timer = document.querySelector('#timer')
-const duracao_foco = 1500; 
-const duracao_descanso_curto = 300; 
-const duracao_descanso_longo = 900;
-
-musica.loop = true
-musica_toggle.addEventListener('change', () => {
-    if(musica.paused){
-        musica.play()
-    } else {
-        musica.pause()
-    }
-})
 
 btn_foco.addEventListener('click', () => {
     alterar_contexto('foco')
@@ -69,3 +50,54 @@ function alterar_contexto(contexto) {
             break;
     }
 }
+
+const musica_toggle = document.querySelector('#alternar-musica')
+const musica = new Audio('./sons/luna-rise-part-one.mp3')
+
+musica.loop = true
+musica_toggle.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
+
+const btn_timer = document.querySelector('#start-pause');
+const timer_start = new Audio('./sons/play.wav');
+const timer_pause = new Audio('./sons/pause.mp3');
+const timer_finalizado = new Audio('./sons/beep.mp3')
+let contagem_tempo = 5
+let contagem_tempo_intervalo = null
+const contagem_tempo_regressiva = () => {
+    if(contagem_tempo <= 0){
+        timer_finalizado.play()
+        alert('Timer Finalalizado!')
+        resetar_contagem_tempo()
+        return
+    }
+    contagem_tempo -= 1
+    console.log('Timer: ' + contagem_tempo)
+}
+
+btn_timer.addEventListener('click', iniciar_contagem_tempo)
+
+function iniciar_contagem_tempo() {
+    if(contagem_tempo_intervalo){
+        timer_pause.play()
+        resetar_contagem_tempo()
+        return
+    }
+    timer_start.play()
+    contagem_tempo_intervalo = setInterval(contagem_tempo_regressiva, 1000)
+}
+
+function resetar_contagem_tempo() {
+    clearInterval(contagem_tempo_intervalo)
+    contagem_tempo_intervalo = null
+}
+
+const timer = document.querySelector('#timer')
+const duracao_foco = 1500; 
+const duracao_descanso_curto = 300; 
+const duracao_descanso_longo = 900;
